ad72de1b51c29cc19fbe07eb731aa86adf22b14e
zjufirefly <zjufirefly@126.com>
2014-11-16 18:00:27 +0800

    test array

--- shell_array.sh
+++ shell_array.sh
@@ -5,11 +5,112 @@ array[2]=2
 array[16]=16
 
 # 输出数组元素
+# result:16
 echo ${array[2]}
 
 # 输出数组元素个数
+# result:3
 echo ${#array[@]}
 
 # 字符串作为key
+# result:aaa
 array[aaa]=aaa
 echo ${array[aaa]}
+
+echo
+
+# result:
+# zzz
+# bbb
+# ccc
+# ddd
+# eee
+array=("zzz" "bbb ccc" "ddd eee")
+for value in ${array[@]}
+do
+    echo $value
+done
+
+echo
+
+# result:
+# zzz
+# bbb ccc
+# ddd eee
+array=("zzz" "bbb ccc" "ddd eee")
+for value in "${array[@]}"
+do
+    echo $value
+done
+
+echo
+
+# result:
+# zzz
+# bbb
+# ccc
+# ddd
+# eee
+array=("zzz" "bbb ccc" "ddd eee")
+for value in ${array[*]}
+do
+    echo $value
+done
+
+echo
+
+# result:
+# zzz bbb ccc ddd eee
+array=("zzz" "bbb ccc" "ddd eee")
+for value in "${array[*]}"
+do
+    echo $value
+done
+
+echo
+
+# 获取数组部分元素
+# result:
+# bbb
+# ccc
+# ddd
+# eee
+array=("zzz" "bbb ccc" "ddd eee")
+for value in ${array[@]:1:2}
+do
+    echo $value
+done
+
+echo
+
+# result
+# bbb ccc
+# ddd eee
+array=("zzz" "bbb ccc" "ddd eee")
+for value in "${array[@]:1:2}"
+do
+    echo $value
+done
+
+echo
+
+# result:
+# bbb
+# ccc
+# ddd
+# eee
+array=("zzz" "bbb ccc" "ddd eee")
+for value in ${array[*]:1:2}
+do
+    echo $value
+done
+
+echo
+
+# result:
+# bbb ccc ddd eee
+array=("zzz" "bbb ccc" "ddd eee")
+for value in "${array[*]:1:2}"
+do
+    echo $value
+done
